a9eeeddc56b1a884f55d2ca9e0cf5a01204c9a94
Vũ Thái Sơn <sonvt195@users.noreply.github.com>
2019-01-31 09:19:19 +0700

    11
    11

---
+++ src/main/java/vn/nhitw/it/qlpb/QlpbApplication.java
@@ -0,0 +1,14 @@
+package vn.nhitw.it.qlpb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlpbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlpbApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/vn/nhitw/it/qlpb/QlpbApplicationTests.java
@@ -0,0 +1,17 @@
+package vn.nhitw.it.qlpb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QlpbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
